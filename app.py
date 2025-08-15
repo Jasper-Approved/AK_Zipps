7606a29499210aee236fd7b227e5cc5a14a0ebc5
6Cats4aDomino <6Cats4aDomino@gmail.com>
2025-08-14 22:59:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,18 @@
 @app.route('/carousel')
 def carousel():
     return render_template('carousel.html')
+
+# Add images in this fashion
+zipper_pulls = [
+    {
+        "image_filename": "glacier-glow.png",
+        "caption": "Glacier Glow",
+        "glyph_class": "fog-shimmer"
+    },
+    {
+        "image_filename": "tundra-twist.png",
+        "caption": "Tundra Twist",
+        "glyph_class": "glyph-overlay"
+    },
+    # Add more pulls here
+]
